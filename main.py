68872fab91119e932e490609277efe705c1d1581
Roberto Quichu <robertoalbo77@gmail.com>
2023-07-18 18:08:45 -0300

    Creacion de la primera Ventana.

---
+++ main.py
@@ -0,0 +1,7 @@
+#Importacion de funciones.
+from customtkinter import * 
+
+#Creacion de la ventana principal.
+ventana_principal = CTk()
+ventana_principal.title("Tour Music")
+ventana_principal.mainloop()
