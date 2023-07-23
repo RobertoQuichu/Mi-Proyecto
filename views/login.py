426e7ad14f968022ee18f904623303362a120290
Roberto Quichu <robertoalbo77@gmail.com>
2023-07-23 18:37:39 -0300

    Batallando con el guardado de datos Parte 2

--- views/login.py
+++ views/login.py
@@ -3,9 +3,9 @@ from tkinter import *
 from CTkMessagebox import CTkMessagebox
 from views.ventana_principal import Aplicacion
 from models.usuarios import Usuario
-import customtkinter, json
+import customtkinter
 
-class Inicio_de_app (customtkinter.CTk) :
+class Inicio_de_app(customtkinter.CTk)  :
 
     """ 
     Esta clase inicia la ventana principal en la cual el usuario ingresara su nombre
@@ -21,7 +21,6 @@ class Inicio_de_app (customtkinter.CTk) :
         self.title("App Music Tour")
         self.geometry("360x330")
         self.iconbitmap("assets\ondas-sonoras.ico")
-        self.lista_usuarios = []
 
         #Centrar ventana.
         window_width = self.winfo_reqwidth()
@@ -46,20 +45,25 @@ class Inicio_de_app (customtkinter.CTk) :
         self.boton_cierre = customtkinter.CTkButton(self, text = "Cerrar Aplicacion", command = self.destroy)
         self.boton_cierre.pack(pady = 5)
         
-    def iniciar_app (self) :
-        
-        """ Este metodo da inicio a las comandos principales de la aplicacion."""
+    def iniciar_app(self):
 
-        #Procesamiento de datos.
+        """Este método da inicio a las comandos principales de la aplicación."""
+
+        # Procesamiento de datos.
         nombre = self.entrada_nombre.get()
         apellido = self.entrada_apellido.get()
-        
-        #Cargamos los archivos existente.
-        if nombre and apellido:
 
-            self.withdraw() #Oculta la ventana actual.
+        # Cargamos los archivos existentes.
+        if nombre and apellido:
+            self.withdraw()  # Oculta la ventana actual.
+
+            # Creamos una instancia de Usuario con los datos ingresados
+            nuevo_usuario = Usuario(id="", nombre=nombre, apellido=apellido, historial=[])
+
+            # Guardamos el nuevo usuario en el archivo JSON
+            nuevo_usuario.guardar_usuario(nuevo_usuario ,"data/nombre_usuario.json")
+
             self.app = Aplicacion(nombre, apellido)
             self.app.protocol("WW_DELETE_WINDOW", self.mostrar_ventana_inicial)
-        
-        else :
-            CTkMessagebox(title = "Campo Vacio", message = "Por favor ingrese sus datos.")
+        else:
+            CTkMessagebox(title="Campo Vacio", message="Por favor ingrese sus datos")
